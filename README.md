1dff1b799ea392ace5bb13f3ecfa3396d945ae24
Alan Nardin <alan.nardin@gmail.com>
2022-05-16 12:52:38 -0300

    Alterações no layout e inclusão de README

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Bingo
 App desktop para acompanhamento de bingo em Python
+
+---
+#### Para executar:
+***Linux***
+```sh
+$ sudo apt-get install python3-tk
+$ git clone https://github.com/alan-nardin/Bingo.git
+$ cd Bingo
+$ python3 app.py
+```
+***Windows***
+```sh
+C:\Projetos> git clone https://github.com/alan-nardin/Bingo.git
+C:\Projetos> cd Bingo
+C:\Projetos> python app.py
+```
+
+---
+### Sceenshots:
+***Linux***
+
+![Linux](screen_linux.gif)
+
+***Windows***
+
+![Windows](screen_windows.gif)
--- app.py
+++ app.py
@@ -3,12 +3,13 @@ import tkinter.messagebox as mbox
 
 window = tk.Tk()
 window.title('Bingo')
-
-botoes = []
+positionRight = int(window.winfo_screenwidth()/2 - 650/2)
+window.geometry(f"650x650+{positionRight}+10")
+window.config(bg='#d9d9d9')
 
 for i, item in enumerate(['B','I','N','G','O']):
-    label = tk.Label(window, text=item, font=('Consolas', 20), fg='#ff0000')
-    label.grid(row=0, column=i+1, sticky='nesw', pady=5)
+    label = tk.Label(window, text=item, font=('Consolas', 20), fg='#ff0000', bg='#d9d9d9')
+    label.grid(row=0, column=i+1, sticky='nesw', pady=3)
 
 
 def pinta_btn(btn: tk.Button):
@@ -20,9 +21,10 @@ def pinta_btn(btn: tk.Button):
     btn.config(bg=bg, activebackground=bg)
     btn.update()
 
+botoes = []
 
 for i in range(1, 76):
-    button = tk.Button(window, text=i, font=('Consolas', 17), pady=30, padx=50)
+    button = tk.Button(window, text=i, font=('Consolas', 17), pady=30, padx=50, bg='#d9d9d9')
     button.config(command=lambda button=button: pinta_btn(button))
     botoes.append(button)
 
@@ -36,7 +38,7 @@ for c in range(1, 6):
         indice+=1
 
 
-label = tk.Label(window, text='Pressione F5 para iniciar um novo jogo', font=('Consolas', 13))
+label = tk.Label(window, text='Pressione F5 para iniciar um novo jogo', font=('Consolas', 13), fg='#0000ff')
 window.rowconfigure(17, weight=2, minsize=30)
 label.grid(row=17, column=1, columnspan=5, sticky='nesw')
 
@@ -52,6 +54,4 @@ def novo_jogo(event):
         btn.update()
 
 window.bind('<F5>', novo_jogo)
-
-window.eval('tk::PlaceWindow . center')
 window.mainloop()
